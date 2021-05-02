1378d3f74bb4e95ccd55ddc8f5179fd0ef3e59ab
jakemidway <jakemidway@gmail.com>
2021-05-02 22:55:52 +0300

    разрушил хеадер

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ let anchors = document.querySelectorAll('a[href*="#"]');
 for(let anchor of anchors){
     anchor.addEventListener('click', function (event) {
         event.preventDefault();
-        openMenu();
         let blockID = anchor.getAttribute('href');
         document.querySelector(blockID).scrollIntoView({
             behavior: 'smooth',
